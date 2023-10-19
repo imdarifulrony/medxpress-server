c3a74ff46378b39fb85327c5ce48f3cbe0e91bc3
Ariful Islam <ariful.rony10@gmail.com>
2023-10-19 18:28:35 +0600

    cors added

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,10 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: 'http://localhost:4200/',
+    credentials: true,
+  });
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
 }

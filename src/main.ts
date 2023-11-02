049fb2792f795efe6c57b78c15cee8656135ff8a
Ariful Islam <ariful.rony10@gmail.com>
2023-11-02 17:08:18 +0600

    config changed

--- src/main.ts
+++ src/main.ts
@@ -8,15 +8,19 @@ import { ValidationPipe } from '@nestjs/common';
 import * as cookieParser from 'cookie-parser';
 // somewhere in your initialization file
 
+// app.enableCors({
+//   origin: [
+//     'https://mymedxpress.netlify.app/',
+//     'http://localhost:4200',
+//     'http://localhost:3000',
+//   ],
+//   credentials: true,
+// });
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: 'https://mymedxpress.netlify.app/',
-    credentials: true,
-  });
-  // app.enableCors();
+  app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
   app.use(cookieParser());
-  await app.listen(3000);
+  await app.listen(3001);
 }
 bootstrap();
